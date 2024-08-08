607a00f54ae9edfc804659d0d66f6cbba0be18b8
Vico <egidiusvico@gmail.com>
2024-08-08 21:59:11 +0700

    finish home start search

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -5,52 +5,68 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(200.0),
-        child: AppBar(
-          title: Column(children: [
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                CircleAvatar(
-                  radius: 20,
-                  backgroundImage:
-                      NetworkImage('https://via.placeholder.com/150'),
-                ),
-                TextButton(onPressed: () {}, child: Text('Saved')),
-                TextButton(onPressed: () {}, child: Text('Liked')),
-                TextButton(onPressed: () {}, child: Text('Created')),
-                IconButton(onPressed: () {}, icon: Icon(Icons.settings))
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search),
-                      hintText: 'Search',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide.none,
+        preferredSize: Size.fromHeight(120.0),
+        child: Container(
+          color: Colors.green[100],
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage:
+                        NetworkImage('https://via.placeholder.com/150'),
+                  ),
+                  TextButton(onPressed: () {}, child: Text('Saved')),
+                  TextButton(onPressed: () {}, child: Text('Liked')),
+                  TextButton(onPressed: () {}, child: Text('Created')),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.settings))
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.search),
+                        hintText: 'Search',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[200],
                       ),
-                      filled: true,
-                      fillColor: Colors.grey[200],
                     ),
                   ),
-                ),
-                IconButton(onPressed: () {}, icon: Icon(Icons.grid_view)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.add))
-              ],
-            )
-          ]),
-          backgroundColor: Colors.green[100],
+                  IconButton(onPressed: () {}, icon: Icon(Icons.grid_view)),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.add))
+                ],
+              ),
+              SizedBox(height: 10), // Adding a SizedBox for spacing
+            ],
+          ),
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [Row()],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+          child: GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, crossAxisSpacing: 10, mainAxisSpacing: 10),
+              itemCount: 20,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  color: Colors.grey[300],
+                  child: Image.network('https://via.placeholder.com/150',
+                      fit: BoxFit.cover),
+                );
+              }),
         ),
       ),
     );
--- lib/view/search_view.dart
+++ lib/view/search_view.dart
@@ -3,8 +3,70 @@ import 'package:flutter/material.dart';
 class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Search Page',style: TextStyle(fontSize: 100)),
-    );
+    return Scaffold(
+        appBar: PreferredSize(
+      preferredSize: Size.fromHeight(120.0),
+      child: Container(
+          color: Colors.green[100],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          hintText: 'Search',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+
+                SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(children: [
+                      InkWell(
+                        child: IconButton(
+                            icon: Icon(Icons.filter), onPressed: () {}),
+                      ),
+                      InkWell(
+                        child: ElevatedButton(
+                            onPressed: () {}, child: Text('Option 1')),
+                      ),
+                      InkWell(
+                        child: ElevatedButton(
+                            onPressed: () {}, child: Text('Option 1')),
+                      ),
+                      InkWell(
+                        child: ElevatedButton(
+                            onPressed: () {}, child: Text('Option 1')),
+                      ),
+                      InkWell(
+                        child: ElevatedButton(
+                            onPressed: () {}, child: Text('Option 1')),
+                      ),
+                      InkWell(
+                        child: ElevatedButton(
+                            onPressed: () {}, child: Text('Option 1')),
+                      ),
+                    ])),
+
+                SizedBox(height: 10), // Adding a SizedBox for spacing
+              ],
+            ),
+          )),
+    ));
   }
 }
