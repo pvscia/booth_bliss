91568e7d5f5b1a1a4f1a0172bd8bdf2f25ea08c7
Patricia Valerie Santoso <21xiepatrice@gmail.com>
2024-08-08 22:18:30 +0700

    connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'view/splash_screen.dart';
-import 'view/login_view.dart';
+import 'test.dart';
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized(); //ensure that the Flutter engine is properly initialized before you run any code that depends on it 
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -10,8 +15,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SplashScreen(),
-      // home: LoginPage(),
+      // home: SplashScreen(),
+      home: AddDataPage(),
     );
   }
 }
