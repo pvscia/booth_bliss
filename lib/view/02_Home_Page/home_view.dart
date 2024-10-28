b76f28e12fcce0556e5723d85468d4b10c86f17a
pvscia <21xiepatrice@gmail.com>
2024-10-28 14:11:25 +0700

    like btn di detail

--- lib/view/02_Home_Page/home_view.dart
+++ lib/view/02_Home_Page/home_view.dart
@@ -4,7 +4,6 @@ import 'package:booth_bliss/view/02_Home_Page/view/header_widget.dart';
 import 'package:booth_bliss/view/02_Home_Page/view/image_grid_widget.dart';
 import 'package:booth_bliss/view/06_Detail_Page/view/detail_main.dart';
 import 'package:flutter/material.dart';
-import 'package:get/route_manager.dart';
 import 'tags_and_slider_widget.dart';
 
 class HomeView extends StatefulWidget {
