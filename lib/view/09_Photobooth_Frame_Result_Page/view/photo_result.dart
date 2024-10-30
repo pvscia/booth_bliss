d73bf9f3cdac21be62990225922cfc81500b7e47
pvscia <21xiepatrice@gmail.com>
2024-10-30 11:49:35 +0700

    when idle for 1 mins, back to main page

--- lib/view/09_Photobooth_Frame_Result_Page/view/photo_result.dart
+++ lib/view/09_Photobooth_Frame_Result_Page/view/photo_result.dart
@@ -1,10 +1,12 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:booth_bliss/view/07_Photobooth_Start_Page/start_view.dart';
 import 'package:booth_bliss/view/09_Photobooth_Frame_Result_Page/controller/photo_result_controller.dart';
 import 'package:booth_bliss/view/Utils/view_dialog_util.dart';
 import 'package:flutter/material.dart';
 
-class PhotoResult extends StatelessWidget {
+class PhotoResult extends StatefulWidget {
   final File framePng;
   final String filename;
 
@@ -12,99 +14,140 @@ class PhotoResult extends StatelessWidget {
       {super.key, required this.framePng, required this.filename});
 
   @override
-  Widget build(BuildContext context) {
-    TextEditingController emailController = TextEditingController();
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Center(child: Text('Photo Result')),
+  PhotoResultState createState() => PhotoResultState();
+}
+
+class PhotoResultState extends State<PhotoResult> {
+  Timer? _idleTimer;
+  TextEditingController emailController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _resetIdleTimer();
+  }
+
+  void _resetIdleTimer() {
+    setState(() {
+      _idleTimer?.cancel();
+      // Create a new timer that navigates after 1 minute (60 seconds)
+      _idleTimer = Timer(Duration(minutes: 1), _navigateToOtherPage);
+    });
+    // Cancel the existing timer if any
+  }
+
+  void _navigateToOtherPage() {
+    // Navigate to another page when idle
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(
+        builder: (context) => PhotoboothStartView(), // The page you want to navigate to
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Image.file(framePng),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  iconSize: 100,
-                  icon: Icon(Icons.email),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: Text('Send Email'),
-                        content: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text('Enter your email:'),
-                            SizedBox(height: 10),
-                            TextField(
-                              controller: emailController,
-                              decoration: InputDecoration(
-                                labelText: 'Email',
-                                border: OutlineInputBorder(),
-                              ),
-                            ),
-                          ],
-                        ),
-                        actions: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          (Route<dynamic> route) => false, // This removes all the previous routes
+    );
+  }
+  
+  @override
+  void dispose() {
+    _idleTimer?.cancel(); // Cancel the timer when the page is disposed
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _resetIdleTimer,
+      onPanUpdate: (_) => _resetIdleTimer(),
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Center(child: Text('Photo Result')),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.file(widget.framePng),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    iconSize: 100,
+                    icon: Icon(Icons.email),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text('Send Email'),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
                             children: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: Text('Close'),
-                              ),
-                              TextButton(
-                                onPressed: () async {
-                                  ViewDialogUtil().showLoadingDialog(context);
-                                  await PhotoResultController().sendEmail(
-                                      emailController.text, filename);
-                                  WidgetsBinding.instance
-                                      .addPostFrameCallback((_) {
-                                    Navigator.of(context).pop();
-                                    Navigator.of(context).pop();
-                                  });
-                                },
-                                child: Text('Send'),
+                              Text('Enter your email:'),
+                              SizedBox(height: 10),
+                              TextField(
+                                controller: emailController,
+                                decoration: InputDecoration(
+                                  labelText: 'Email',
+                                  border: OutlineInputBorder(),
+                                ),
                               ),
                             ],
                           ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-                SizedBox(height: 20),
-                IconButton(
-                  iconSize: 100,
-                  icon: Icon(Icons.qr_code),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: Text('QR Code'),
-                        content: Image.asset('assets/img.png'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Text('Close'),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ],
+                          actions: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text('Close'),
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+                                    ViewDialogUtil().showLoadingDialog(context);
+                                    await PhotoResultController().sendEmail(
+                                        emailController.text, widget.filename);
+                                    WidgetsBinding.instance
+                                        .addPostFrameCallback((_) {
+                                      Navigator.of(context).pop();
+                                      Navigator.of(context).pop();
+                                    });
+                                  },
+                                  child: Text('Send'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  IconButton(
+                    iconSize: 100,
+                    icon: Icon(Icons.qr_code),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text('QR Code'),
+                          content: Image.asset('assets/img.png'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('Close'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
